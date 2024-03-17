c6f4b1582f38e39da2b7aee2033cc170c7ad8aab
Maks Mavritskiy <drmax782@gmail.com>
2024-03-17 14:03:40 +0500

    Подготавливает проект к защите.

--- js/modal.js
+++ js/modal.js
@@ -1,12 +1,12 @@
-// Dialog 
+// Dialog
 const modalOpen = document.querySelector(".modal-open")
 const modalClose = document.querySelector(".modal-button-close")
 const dialog = document.querySelector(".modal-window")
 
 modalOpen.addEventListener("click", function() {
-    dialog.showModal()
+  dialog.showModal()
 })
 
 modalClose.addEventListener("click", function() {
-    dialog.close()
+  dialog.close()
 })
